2e75308f250b279da77f1d031e73555243dac954
Dat Nguyen <datnguyen01122003@gmail.com>
2022-09-30 19:38:52 +0700

    update

--- main.js
+++ main.js
@@ -1,12 +1,21 @@
 const openNav = document.querySelector('.fa-bars');
 const closeNav = document.querySelector('.fa-xmark');
 const nav = document.querySelector('.links')
+const overlay = document.querySelector('.overlay');
+console.log(overlay);
 console.log(openNav);
 
 openNav.addEventListener('click', ()=>{
   nav.style.right = '0';
+  overlay.style.display = 'block'
 })
 
 closeNav.addEventListener('click', ()=>{
   nav.style.right = '-30%';
+  overlay.style.display = 'none'
+})
+
+overlay.addEventListener('click', ()=>{
+  nav.style.right = '-30%';
+  overlay.style.display = 'none'
 })
